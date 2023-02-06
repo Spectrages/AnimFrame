6fdc066ce30c386d85197236af4d9db469641271
unknown <spectrages@gmail.com>
2023-02-07 00:47:08 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ let rAF = window.requestAnimationFrame;
 let animationRequest, count;
 let start = null;
 let path = articleWidth - 75;
-let revPath = path * -1;
 let step = -0.05;
 
 const move = (timestamp) => {
@@ -16,14 +15,31 @@ const move = (timestamp) => {
     animationRequest = rAF(move)
     let progress = (timestamp - start);
     count = Math.min(step * progress, path);
-    if(Math.trunc(count) === revPath) {
+    if(Math.trunc(count) === (path * -1)) {
         cancelAnimationFrame(animationRequest)
     }
     square.style.transform = `translateX(${count}px)`;
 }
 rAF(move);
+
+
+
+
+
+
+
+
+
+
 //progress - время, всегда растёт
 //count = фактическая точка начала и конца пути
 //step = шаг
 //path = стартовая точка
 //revPath = целевая точка
+
+/*
+    if(Math.trunc(count) === revPath) {
+        console.log(progress)
+        cancelAnimationFrame(animationRequest)
+    }
+*/
